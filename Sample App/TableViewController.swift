a9513d6d8b892cccd1c36b043e03d1b9dc19a4bc
khushiduddi <kduddi2@illinois.edu>
2021-06-16 18:12:47 -0700

    Table view works.

---
+++ Sample App/TableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TableViewController.swift
+//  Sample App
+//
+//  Created by Khushi Duddi on 6/16/21.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let questions = ["What", "Who", "When", "Where", "How", "Why"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    // How many rows?
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questions.count
+    }
+
+    // What goes in each row?
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+
+        cell.textLabel?.text = questions[indexPath.row]
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "Example", sender: nil)
+    }
+
+}
